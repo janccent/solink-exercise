0a3744a593861923b2b8ecf1cc730dde037b4f22
Jane Peens <jane@hubub.com>
2016-08-26 21:10:16 -0400

    added dummy api call to 'retrieve' answer details for accordion content (updated controller accordingly); added grid-type structure to template to display some data for accordion content

--- app/components/questions/questionsController.js
+++ app/components/questions/questionsController.js
@@ -20,6 +20,10 @@ define([
 
             var currentPage = 1;
 
+            function getDate(timestamp) {
+                return new Date(timestamp * 1000)
+            }
+
             function addQuestions() {
                 console.log('CURRENT PAGE=' + currentPage);
                 QuestionsApiService.getQuestions(currentPage).then(
@@ -27,6 +31,10 @@ define([
                         var questions = response.items;
 
                         questions.forEach( function(element) {
+                            // Convert dates (this could also have been done in the template with a custom filter)
+                            element.last_activity_date = getDate(element.last_activity_date);
+                            element.creation_date = getDate(element.creation_date);
+
                             $scope.questions.push(element);
                         });
                     }
@@ -49,6 +57,20 @@ define([
             });
 
             addQuestions();
+
+            $scope.answers = {};
+
+            $scope.getDetail = function(questionId) {
+                console.log('get detail');
+                if ( ! $scope.answers.hasOwnProperty(questionId) || $scope.answers[questionId] == null ) {
+                    console.log('no answer so');
+                    QuestionsApiService.getAnswer(questionId).then(
+                        function(response) {
+                            $scope.answers[questionId] = response.items[0];
+                        }
+                    );
+                }
+            };
 
         }
     ]);
